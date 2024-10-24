de9033df53d5b3b549e76d465b9c30f8853090f0
James Bingen <james@cpthuba.dev>
2024-10-24 08:52:29 +0200

    Fixed instruction bugs, SDL added, Main loop. Kinda works but still need to figure out drawing less times

--- cmd/emulator/main.go
+++ cmd/emulator/main.go
@@ -1,5 +1,47 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"Chip8toGo/internal/chip8"
+	"Chip8toGo/internal/utils"
+)
+
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %s <Scale> <ROM>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	videoScale, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Invalid scale:", os.Args[1])
+		os.Exit(1)
+	}
+
+	romFilename := os.Args[2]
+
+	cpu := chip8.NewChip8()
+
+	if err := utils.LoadROMFile(cpu, romFilename); err != nil {
+		panic(err)
+	}
+
+	display, err := chip8.NewDisplay("Chip8 - Emulator", chip8.VIDEO_WIDTH, chip8.VIDEO_HEIGHT, int32(videoScale))
+	if err != nil {
+		panic(err)
+	}
+	defer display.CleanUp()
+
+	quit := false
+
+	for !quit {
+		quit = chip8.ProcessInput(cpu)
+		cpu.Cycle()
+
+		display.Update(cpu)
+	}
 
 }
